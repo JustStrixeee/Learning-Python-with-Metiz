aaafddf0d4c0dc37e1efcbb638e7d85f75002d0b
JustStrixeee <basi47lio@gmail.com>
2026-09-20 18:06:00 +0300

    writed basic methods for work with lists

--- lists.py
+++ lists.py
@@ -61,3 +61,59 @@ popped_motorcycles = motorcycles.pop()
 print(popped_motorcycles)
 print(motorcycles)
 
+last_owned = motorcycles.pop()
+print(f"The last motorcycle I owned was a {last_owned.title()}.")
+print(motorcycles)
+
+"""Удаление элементов из произвольной позиции списка"""
+
+first_owned = motorcycles.pop(0)
+print(f"The first motorcycle I owned was a {first_owned.title()}")
+
+"""Если собираешься удалить элемент из списка полностью без эксплуатации, то нужно del. 
+Если нужно использовать элемент далее то pop()"""
+
+"""Удаление элемента по значению"""
+# возьмем отдельный список для наглядности c _
+
+motorcycles_ = ['honda', 'ducati', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles_)
+motorcycles_.remove('ducati')
+print(motorcycles_) # обратим внимание на то, что отсчет шел слева направо и был удален один ducati а еще один остался
+"""проверим еще раз - если улучшить вариант по удалению всех эл. то нужно использовать циклы"""
+motorcycles_.remove('ducati')
+print(motorcycles_) # ['honda', 'yamaha', 'suzuki']
+
+too_expensive = 'yamaha'
+motorcycles_.remove(too_expensive)
+print(motorcycles_)
+print(f"\nA {too_expensive.title()} is too expensive for me.")
+
+"""Упорядочение списка - Python представляет несколько способов упорядочения в зависимости от необходимости"""
+
+"""Метод sort() позволяет изменить список навсегда"""
+
+cars = ["bmw", "audi", "toyota", "subaru"]
+cars.sort()
+print(cars) # ['audi', 'bmw', 'subaru', 'toyota']
+cars.sort(reverse=True)
+print(cars) # ['toyota', 'subaru', 'bmw', 'audi']
+
+"""Метод sorted() позволяет выполнить временную сортировку"""
+cars_ = ["bmw", "audi", "toyota", "subaru"]
+print(f"Here is the original list: {cars_}")
+print(f"Here is the sorted list: {sorted(cars_)}")
+print(f"Here is reverse the sorted list: {sorted(cars_, reverse=True)}")
+
+"""Вывод списка в обратном порядке"""
+# Метод reverse не сортирует в обратном порядке, он меняет порядок списка на обратный (индексы)
+print(cars_) # ['bmw', 'audi', 'toyota', 'subaru']
+
+cars_.reverse()
+print(cars_)
+
+"""Определение длины списка """
+
+print(len(cars_)) # 4
+
+
